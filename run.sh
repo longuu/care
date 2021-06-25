19224cd8b934e9e35292eaf376cb543df6b539bd
银龙 <house.li@dfgroup.pro>
2021-06-25 14:09:20 +0800

    再看看大调

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
-#!/bin/bash
-pwd
-ls
+#!/usr/bin/env bash
+group_name='app'
+app_name='care'
+app_version='0.1'
+#profile_active='prod'
+echo '----copy jar----'
+docker stop ${app_name}
+echo '----stop container----'
+docker rm ${app_name}
+echo '----rm container----'
+docker rmi ${group_name}/${app_name}:${app_version}
+echo '----rm image----'
+docker build -t ${group_name}/${app_name}:${app_version} .
+echo '----build image----'
+docker run -p 8080:8080 --name ${app_name}  --net docker-mynet --ip 172.172.0.88 \
+-e TZ="Asia/Shanghai" \
+-v /etc/localtime:/etc/localtime \
+-v /mydata/app/${app_name}/logs:/var/logs \
+-d ${group_name}/${app_name}:${app_version} \
+echo '----start container----'
