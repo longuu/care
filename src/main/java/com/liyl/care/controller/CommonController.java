a5465d9f67bd9d516bf7191cd69d37df4e02bbfc
longuu <liyl_mep@126.com>
2021-06-27 01:52:10 +0800

    ddddaaaaa222

--- src/main/java/com/liyl/care/controller/CommonController.java
+++ src/main/java/com/liyl/care/controller/CommonController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 @RequestMapping(value = "/common")
 public class CommonController {
@@ -16,12 +14,12 @@ public class CommonController {
     private Environment environment;
 
     @RequestMapping("/all")
-    public String all(){
+    public String all() {
         return "hello world";
     }
 
     @RequestMapping(value = "/hook", method = RequestMethod.POST)
-    public String env(){
+    public String env() {
         return environment.getProperty("dd");
     }
 }
