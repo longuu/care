faba1a5bee01330a364bc3bd64f221ac9c19a810
longuu <liyl_mep@126.com>
2021-06-27 01:41:10 +0800

    继续测试8

--- src/main/java/com/liyl/care/controller/CommonController.java
+++ src/main/java/com/liyl/care/controller/CommonController.java
@@ -1,15 +1,26 @@
 package com.liyl.care.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value = "/common")
 public class CommonController {
 
+    @Autowired
+    private Environment environment;
 
     @RequestMapping("/all")
     public String all(){
         return "hello world";
+    }
+
+    @RequestMapping("/hook")
+    public String env(HttpServletRequest request){
+        return environment.getProperty("dd");
     }
 }
